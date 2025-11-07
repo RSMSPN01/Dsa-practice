136b05c88b9c5f7f488d9425da700c45ae9ef048
RSMSPN01 <praveenikhilbrohters@gmail.com>
2025-11-07 12:02:14 +0530

    Inserting a node into the head

--- 10 Linked List/Lec1LL/insertnode.cpp
+++ 10 Linked List/Lec1LL/insertnode.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+// Definition of singly linked list:
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode(int data1)
+//     {
+//         val = data1;
+//         next = NULL;
+//     }
+//     ListNode(int data1, ListNode *next1)
+//     {
+//         val = data1;
+//         next = next1;
+//     }
+// };
+
+class Solution {
+public:
+    ListNode* insertAtHead(ListNode*& head, int X) {
+
+    }
+};
+int main() {
+
     return 0;
 }
