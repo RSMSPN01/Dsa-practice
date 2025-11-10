9049c9d4392e81621795f0e3c833ed3fe18f8b63
RSMSPN01 <praveennikhilbrothers@gmail.com>
2025-11-10 18:10:17 +0530

    reverse the dll using vector

---
+++ 10 Linked List/Lec2DLL/basics.cpp
@@ -0,0 +1,83 @@
+// These all are the basics for the DLL
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+        prev = NULL;
+    }
+    void print(Node* head) {
+        Node* temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+    // insert node at head
+    Node* insertHead(Node* head, int x) {
+        if (head == NULL)
+        {
+            Node* temp = new Node(x);
+            return temp;
+        }
+        Node* temp = new Node(x);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+        return head;
+    }
+    //Deleting first node
+    Node* deletefNode(Node* head) {
+        if (head->next == NULL && head->prev == NULL)
+        {
+            return NULL;
+        }
+        Node* temp = head;
+        head = temp->next;
+        head->prev = NULL;
+        delete temp;
+        return head;
+    }
+    // Reverse is done using extra space (vector) so the space complexity = O(n).
+    // and we have traverse the LL twice so overall time Complexity is also = O(n).
+    void reverseNode(Node* head) {
+        Node* temp = head;
+        vector<int>v;
+        while (temp != NULL)
+        {
+            v.push_back(temp->data); // stored into the vector
+            temp = temp->next;
+        }
+        reverse(v.begin(), v.end());
+        int i = 0;
+        Node* temp2 = head;
+        while (temp2 != NULL)
+        {
+
+            temp2->data = v[i++];
+            temp2 = temp2->next;
+        }
+    }
+};
+int main() {
+    Node* head = new Node(10);
+    Node* temp = new Node(20);
+    Node* temp1 = new Node(30);
+    head->next = temp;
+    temp->prev = head;
+    temp->next = temp1;
+    temp1->prev = temp;
+    head = head->insertHead(head, 5);
+    head = head->deletefNode(head);
+    head->reverseNode(head);
+    head->print(head);
+    return 0;
+}
