deaef1ef8a8fbdcf897ae12e731e8108ab350007
RSMSPN01 <praveennikhilbrothers@gmail.com>
2025-11-27 09:57:09 +0530

    Efficient way to solve the problem

--- 10 Linked List/Lec3Medium/deletedLL.cpp
+++ 10 Linked List/Lec3Medium/deletedLL.cpp
@@ -50,32 +50,43 @@ public:
         if (head == nullptr) {
             return nullptr;
         }
-        Node* temp = head;
-        while (temp != nullptr)
+        Node* curr = head;
+        while (curr != nullptr)
         {
-            // check for the head first if same them remove that seprately
-            if (temp->data == target && temp == head) {
-                // it means pointing to head node
-                Node* temp2 = temp;
-                head = head->next;
-                temp = temp->next;
-                delete temp2;
-            } else if (temp->data == target && temp->next == nullptr) {
-                // it means we are at the last node of ll 
-                // here memory leak in not handled
-                Node* temp2 = temp;
-                temp->prev->next = nullptr;
-                temp = temp->next;
-                delete temp2;
-            } else if (temp->data == target) {
-                // handle memory leak
-                temp->prev->next = temp->next;
-                temp = temp->next;
+            // checking for head first if head and current is same then move head and current both ahead by one
+            if (curr->data == target && curr == head) {
+                Node* temp = head; // make a temp node to point the node to remove.
+                head = head->next; // move head and current one step ahead
+                curr = curr->next;
+                head->prev = nullptr; // make sure to add this line other wise it is a dangling pointer.
+                delete temp; // free the memory
+            } else if (curr->data == target && curr->next == nullptr) {
+                // it means we are standing at the tail which need to be removed from the LL
+                curr->prev->next = nullptr; // make the prev of last as nullptr
+                delete curr; // free the memory for the tail node. // it will not delete the pointer but only where it is pointing to
+                curr = nullptr; // and safely end the code.
+            } else if (curr->data == target) {
+                // it is the middle of LL 
+                Node* nextNode = curr->next; // store the value where we should move ahead.
+                curr->prev->next = nextNode; // change the previous node and point it to the next node of the current node
+                nextNode->prev = curr->prev; // change the next node's prev and point it to the currn node's prev.
+                delete curr; // free the memory 
+                curr = nextNode; // and move ahead to the next node.
             } else {
-                temp = temp->next;
+                curr = curr->next;
             }
         }
         return head;
+    }
+    void printReverse(Node* head) {
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
     }
 };
 int main() {
@@ -90,6 +101,6 @@ int main() {
     head = head->insertHead(head, 5);
     head = head->insertHead(head, 5);
     head = head->deleteOccurences(head, 5);
-    head->print(head);
+    head->printReverse(head);
     return 0;
 }
