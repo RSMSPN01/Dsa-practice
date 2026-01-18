d105fc2d31e7c087550fda7a4f42a949a43e28ea
RSMSPN01 <praveennikhilbrothers@gmail.com>
2026-01-18 15:59:24 +0530

    Question done in less then 20 minutes

---
+++ Striver/Binary Search/Medium/nthRoot.cpp
@@ -0,0 +1,31 @@
+// Approach : Naive way is by using linear search method but we can do faster by using bs search
+// This is similar to the finding root of the number just a few twicks and we are good to go.
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int NthRoot(int N, int M) {
+    // edge case to handle
+    if (N == 1) {
+        return M;
+    }
+    int start = 0, end = M, mid = 0;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        if (pow(mid, N) == M)
+        {
+            return mid;
+        } else if (pow(mid, N) > M) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+int main() {
+    int n = 4, m = 81;
+    cout << NthRoot(n, m);
+    return 0;
+}
