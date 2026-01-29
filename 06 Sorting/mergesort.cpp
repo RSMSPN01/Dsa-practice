4c85c67d6fd7e83031b8cc079b009336252b388f
RSMSPN01 <praveennikhilbrothers@gmail.com>
2026-01-29 16:03:46 +0530

    sample code for merge is updated

--- 06 Sorting/mergesort.cpp
+++ 06 Sorting/mergesort.cpp
@@ -1,46 +1,83 @@
 #include <iostream>
 using namespace std;
+// for futute use this code
+
+// void merge(int arr[], int low, int high) {
+// int mid = (low + high) /2;
+//   int n = high - low + 1;
+//   int temp[n];
+//   int i = low;
+//   int j = mid + 1;
+//   int k = 0;
+
+//   while (i <= mid && j <= high) {
+//     if (arr[i] <= arr[j]) {
+//       temp[k++] = arr[i++];
+//     } else {
+//       temp[k++] = arr[j++];
+//     }
+//   }
+
+//   while (i <= mid) {
+//     temp[k++] = arr[i++];
+//   }
+
+//   while (j <= high) {
+//     temp[k++] = arr[j++];
+//   }
+
+//   for (int l = 0; l < n; l++) {
+//     arr[low + l] = temp[l];
+//   }
+// }
+
+// void mergeSort(int arr[], int low, int high) {
+//   if (low < high) {
+//     int mid = low + (high - low) / 2;
+//     mergeSort(arr, low, mid);
+//     mergeSort(arr, mid + 1, high);
+//     merge(arr, low, high);
+//   }
+// }
+
 void merge(int arr[], int start, int mid, int end) {
-    int n = end - start + 1;
-    int temp[n], i = start, j = mid + 1, k = 0; // give proper values to the variable i and j 
-    while (i <= mid && j <= end)
+  int n = end - start + 1;
+  int temp[n], i = start, j = mid + 1,
+               k = 0; // give proper values to the variable i and j
+  while (i <= mid && j <= end) {
+    if (arr[i] <= arr[j]) // to ensure the stability do this though
     {
-        if (arr[i] <= arr[j])// to ensure the stability do this though
-        {
-            temp[k++] = arr[i++];
-        } else {
-            temp[k++] = arr[j++];
-        }
+      temp[k++] = arr[i++];
+    } else {
+      temp[k++] = arr[j++];
     }
-    while (i <= mid)
-    {
-        temp[k++] = arr[i++];
-    }
-    while (j <= end) {
-        temp[k++] = arr[j++];
-    }
-    for (int l = 0; l < n; l++)
-    {
-        arr[l + start] = temp[l]; // please remember this one use start because start is not always starts from index zero
-    }
+  }
+  while (i <= mid) {
+    temp[k++] = arr[i++];
+  }
+  while (j <= end) {
+    temp[k++] = arr[j++];
+  }
+  for (int l = 0; l < n; l++) {
+    arr[l + start] = temp[l]; // please remember this one use start because
+                              // start is not always starts from index zero
+  }
 }
 void mergeSort(int arr[], int start, int end) {
-    if (start < end)
-    {
-        int mid = start + (end - start) / 2;
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid + 1, end);
-        merge(arr, start, mid, end);
-    }
+  if (start < end) {
+    int mid = start + (end - start) / 2;
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, start, mid, end);
+  }
 }
 int main() {
-    int n = 9;
-    int arr[n] = { 23,12,9,21,8,34,10,17,37 };
-    mergeSort(arr, 0, n - 1);
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+  int n = 9;
+  int arr[n] = {23, 12, 9, 21, 8, 34, 10, 17, 37};
+  mergeSort(arr, 0, n - 1);
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
 
-    return 0;
+  return 0;
 }
