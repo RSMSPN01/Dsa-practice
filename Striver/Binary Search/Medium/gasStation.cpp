7c0c22fd32cf118147e38d9d06065c9d0c3dde8a
RSMSPN01 <praveennikhilbrothers@gmail.com>
2026-02-02 16:04:15 +0530

    Look after the whole course is finished

--- Striver/Binary Search/Medium/gasStation.cpp
+++ Striver/Binary Search/Medium/gasStation.cpp
@@ -1,7 +1,8 @@
 // Approach : Brute force : So we will do a linear search on that where, the
 // loop starts from the min distance between two points divided by 2 bcs that
 // could be the most min possible answer and it will goes till arr[max] -
-// arr[min].
+// arr[min]. // don't even be able to think for the brute force i don't think
+// so this will even work i have just wrote it down but this will not work.
 
 // Better Approach : We know we have a range to find the answer so we can apply
 // that its a same question of pattern min of max just data types has changed
@@ -11,11 +12,16 @@
 // There is also a little wierd thing in question, the approach i am going to
 // write is that we can only put one gas station in between the two consecutive
 // stations
+
 // After thinking on it i find out i am wrong, if we will start putting
 // one or even more gas station in between then, we will not be able to find the
 // max as our output bcs the value is just going to get down and even down,
 // instead putting more in between directly put them in the end please think
 // about why i am saying this you will get to know that
+
+// I am trying to solve this question for so much long time still not be able to
+// think for the valid function how i am going to write that lets see what can i
+// learn from video and try to implement that here.
 
 #include <algorithm>
 #include <iostream>
@@ -23,18 +29,22 @@
 
 using namespace std;
 bool canPlace(int mid, vector<int> &nums, int k) {
-  int count = 0, lastCount = 0;
-  for (int i = 1; i < nums.size(); i++) {
-    if (nums[i] - lastCount >= mid) {
-      count++;
-      lastCount = i;
-    }
-    // can use early stopping but hardly makes the code faster
-  }
-  if (count >= k) {
-    return true;
-  }
-  return false;
+  // This approach which i have write here, there is no single way this could
+  // work it will always return the ans as false idk why i have even written
+  // this
+
+  // int count = 0, lastCount = 0;
+  // for (int i = 1; i < nums.size(); i++) {
+  //   if (nums[i] - lastCount >= mid) {
+  //     count++;
+  //     lastCount = i;
+  //   }
+  //   // can use early stopping but hardly makes the code faster
+  // }
+  // if (count >= k) {
+  //   return true;
+  // }
+  // return false;
 }
 long double minimiseMaxDistance(vector<int> &nums, int k) {
   int n = nums.size();
@@ -61,8 +71,8 @@ long double minimiseMaxDistance(vector<int> &nums, int k) {
   return high;
 }
 int main() {
-  vector<int> nums = {1, 5};
-  int k = 1;
+  vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int k = 10;
   cout << minimiseMaxDistance(nums, k);
   return 0;
 }
