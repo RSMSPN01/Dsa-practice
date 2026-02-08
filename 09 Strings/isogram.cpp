ef1b6053742906df58e8c22f9eb7dc53bcab8d7b
RSMSPN01 <praveennikhilbrothers@gmail.com>
2026-02-08 23:05:22 +0530

    formating changes in the code with only one side maping (wrong logic)

--- 09 Strings/isogram.cpp
+++ 09 Strings/isogram.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
-bool isogramOrNot(string& s) {
-    bool freq[26] = { false };
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (freq[s[i] - 'a'] == true) {
-            return false;
-        } else {
-            freq[s[i] - 'a'] = true;
-        }
+bool isogramOrNot(string &s) {
+  bool freq[26] = {false};
+  for (int i = 0; i < s.length(); i++) {
+    if (freq[s[i] - 'a'] == true) {
+      return false;
+    } else {
+      freq[s[i] - 'a'] = true;
     }
-    return true;
+  }
+  return true;
 }
 int main() {
-    string s = "machine";
-    cout << isogramOrNot(s);
-    return 0;
+  string s = "machine";
+  cout << isogramOrNot(s);
+  return 0;
 }
