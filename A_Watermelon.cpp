c7f4855de8c47ab3cdbbd1970968f8fe35760ac6
RSMSPN01 <praveennikhilbrothers@gmail.com>
2026-03-12 16:03:22 +0530

    1st cp problem, still in testing

---
+++ A_Watermelon.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+bool canDivide(int n) {
+  if (n < 2) {
+    return false;
+  }
+  if (n % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+int main() {
+  // your code goes here
+  int n;
+  cin >> n;
+  if (canDivide(n)) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+}
